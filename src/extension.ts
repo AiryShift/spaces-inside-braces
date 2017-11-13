a5399d64bc85fe5e3441855140c854e14460171b
AiryShift <religionofaperturelabs@gmail.com>
2017-11-13 13:58:47 +1100

    Fix bug where spacing doesn't work at start of line
    The "line" is calculated at the start of space consideration so it stays
    the same even if a spacing occurs. This is possibly not necessary.
    There is also a bounds check on which characters to slice.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 'use strict';
 import * as vscode from 'vscode';
+import { connect } from 'net';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -15,36 +16,26 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
+function getPosition(): vscode.Position {
+    return vscode.window.activeTextEditor.selection.active;
+}
+
 class Spacer {
 
-    public shouldSpace(open: string, close: string): boolean {
-        const editor = vscode.window.activeTextEditor;
-        if (!editor || !editor.selection.isEmpty) {
-            return;
-        }
-
-        const position = editor.selection.active;
-
-        const positionBeforeStart = new vscode.Position(position.line, position.character - 1);
-        const positionBeforeEnd = new vscode.Position(position.line, position.character + 1);
-        const positionAfterStart = new vscode.Position(position.line, position.character + 1);
-        const positionAfterEnd = new vscode.Position(position.line, position.character + 2);
+    public shouldSpace(open: string, close: string, line: string): boolean {
+        const position = getPosition();
 
         // two characters before cursor
-        const textBefore = editor.document.getText(new vscode.Range(positionBeforeStart, positionBeforeEnd));
+        const textBefore = line.slice(Math.max(position.character - 1, 0), position.character + 1);
         // one chrarcter after cursor
-        const textAfter = editor.document.getText(new vscode.Range(positionAfterStart, positionAfterEnd));
+        const textAfter = line.slice(position.character + 1, position.character + 2);
 
         return textBefore === `${open} ` && textAfter === close;
     }
 
     public space() {
         const editor = vscode.window.activeTextEditor;
-        if (!editor || !editor.selection.isEmpty) {
-            return;
-        }
-
-        const position = editor.selection.active;
+        const position = getPosition();
 
         // add the space
         editor.edit(function (edit) {
@@ -57,34 +48,21 @@ class Spacer {
         editor.selection = newSelection;
     }
 
-    public shouldUnspace(open: string, close: string): boolean {
+    public shouldUnspace(open: string, close: string, line: string): boolean {
         const editor = vscode.window.activeTextEditor;
-        if (!editor || !editor.selection.isEmpty) {
-            return;
-        }
-
-        const position = editor.selection.active;
-
-        const positionBeforeStart = new vscode.Position(position.line, position.character - 2);
-        const positionBeforeEnd = new vscode.Position(position.line, position.character - 1);
-        const positionAfterStart = new vscode.Position(position.line, position.character - 1);
-        const positionAfterEnd = new vscode.Position(position.line, position.character + 1);
+        const position = getPosition();
 
         // one character before cursor
-        const textBefore = editor.document.getText(new vscode.Range(positionBeforeStart, positionBeforeEnd));
+        const textBefore = line.slice(Math.max(position.character - 2, 0), Math.max(position.character - 1, 0));
         // two characters after cursor
-        const textAfter = editor.document.getText(new vscode.Range(positionAfterStart, positionAfterEnd));
+        const textAfter = line.slice(Math.max(position.character - 1, 0), position.character + 1);
 
         return textBefore === open && textAfter === ` ${close}`;
     }
 
     public unspace() {
         const editor = vscode.window.activeTextEditor;
-        if (!editor || !editor.selection.isEmpty) {
-            return;
-        }
-
-        const position = editor.selection.active;
+        const position = getPosition();
 
         // remove the extra space
         editor.edit(function (edit) {
@@ -123,24 +101,32 @@ class SpacerController {
         this._disposable.dispose();
     }
 
-    private _considerSpacingFor(open: string, close: string) {
-        if (this._spacer.shouldSpace(open, close)) {
+    private _considerSpacingFor(open: string, close: string, line: string) {
+        if (this._spacer.shouldSpace(open, close, line)) {
             this._spacer.space();
-        } else if (this._spacer.shouldUnspace(open, close)) {
+        } else if (this._spacer.shouldUnspace(open, close, line)) {
             this._spacer.unspace();
         }
     }
 
     private _onDidChangeTextDocument() {
         if (this._config.get("enable", true)) {
+            const editor = vscode.window.activeTextEditor;
+            if (!editor || !editor.selection.isEmpty) {
+                return;
+            }
+
+            const position = editor.selection.active;
+            const line = editor.document.lineAt(position.line).text;
+
             if (this._config.get("enableForBraces", true)) {
-                this._considerSpacingFor("{", "}");
+                this._considerSpacingFor("{", "}", line);
             }
             if (this._config.get("enableForParens", true)) {
-                this._considerSpacingFor("(", ")");
+                this._considerSpacingFor("(", ")", line);
             }
-            if (this._config.get("enableForBrackets"), true) {
-                this._considerSpacingFor("[", "]");
+            if (this._config.get("enableForBrackets", true)) {
+                this._considerSpacingFor("[", "]", line);
             }
         }
     }
